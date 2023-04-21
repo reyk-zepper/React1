10840ec17b84c9d1ef50b6c7ee6c9ca8d11908eb
Reyk Zepper <reyk.zepper@gmail.com>
2023-04-21 15:43:09 +0200

    implement counter stops at 0

--- src/components/Count.js
+++ src/components/Count.js
@@ -1,15 +1,20 @@
 import React from "react";
 import styled from "styled-components";
+import { useState } from "react";
 
 export default function Counter() {
-  const [count, setCount] = React.useState(0);
-
+  const [count, setCount] = useState(0);
+  const decrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
   return (
     <StyledCounter>
       <h1>Counter</h1>
       <p>{count}</p>
       <button onClick={() => setCount(count + 1)}>+</button>
-      <button onClick={() => setCount(count - 1)}>-</button>
+      <button onClick={decrement}>-</button>
     </StyledCounter>
   );
 }
@@ -31,5 +36,11 @@ const StyledCounter = styled.div`
   button {
     font-size: 2rem;
     margin: 1rem;
+  }
+  p {
+    font-size: 4rem;
+    border: 1px solid white;
+    border-radius: 1rem;
+    padding: 1rem;
   }
 `;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,39 @@
 import React from "react";
+import styled from "styled-components";
 
 export default function Header() {
   return (
-    <>
+    <StyledHeader>
       <ul>
         <li>Home</li>
         <li>Counter</li>
         <li>About</li>
       </ul>
-    </>
+    </StyledHeader>
   );
 }
+
+const StyledHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 10vh;
+  width: 100vw;
+  background-color: coral;
+  color: white;
+  font-size: 2rem;
+  text-align: center;
+  ul {
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    li {
+      padding: 1rem;
+      cursor: pointer;
+    }
+  }
+`;
