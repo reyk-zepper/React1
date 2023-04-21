139f6f9283d342bb949d26cb5f299273d45a466e
Reyk Zepper <reyk.zepper@gmail.com>
2023-04-21 14:26:56 +0200

    header component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Header from "./Header";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>hallo world!</h1>);
+root.render(<Header />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
