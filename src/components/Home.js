cb26fadffbbf2a2b64db693f1b1024081aa39e65
Reyk Zepper <reyk.zepper@gmail.com>
2023-04-21 18:13:43 +0200

    useNavigate

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/counter");
+  };
+
   return (
     <div>
       <h1>you're HOME now</h1>
+      <button onClick={handleClick}>click me to the COUNTER</button>
     </div>
   );
 }
