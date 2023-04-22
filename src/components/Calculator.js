ef6b4d78770c0d1a63f4d9c6f477d224d309cb2c
Reyk Zepper <reyk.zepper@gmail.com>
2023-04-22 11:28:45 +0200

    add handleEqual

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,29 +1,52 @@
 import React from "react";
 import styled from "styled-components";
+import { useState } from "react";
+import * as math from "mathjs";
 
 export default function Calculator() {
+  const [result, setResult] = useState("");
+
+  const handleClick = (e) => {
+    const value = e.target.value;
+    setResult(result.concat(value));
+  };
+
+  const handleClear = () => {
+    setResult("");
+  };
+
+  const handleEqual = () => {
+    try {
+      const evaluatedResult = math.evaluate(result);
+      setResult(evaluatedResult.toString());
+    } catch (error) {
+      setResult("Invalid input");
+    }
+  };
+
   return (
     <StyledCalculator>
-      <input type="text" placeholder="0" id="answer" />
-      <input type="button" value="9" />
-      <input type="button" value="8" />
-      <input type="button" value="7" />
-      <input type="button" value="6" />
-      <input type="button" value="5" />
-      <input type="button" value="4" />
-      <input type="button" value="3" />
-      <input type="button" value="2" />
-      <input type="button" value="1" />
+      <input type="text" placeholder="0" id="answer" value={result} />
+      <input type="button" value="9" onClick={handleClick} />
+      <input type="button" value="8" onClick={handleClick} />
+      <input type="button" value="7" onClick={handleClick} />
+      <input type="button" value="6" onClick={handleClick} />
+      <input type="button" value="5" onClick={handleClick} />
+      <input type="button" value="4" onClick={handleClick} />
+      <input type="button" value="3" onClick={handleClick} />
+      <input type="button" value="2" onClick={handleClick} />
+      <input type="button" value="1" onClick={handleClick} />
 
-      <input type="button" value="0" />
-      <input type="button" value="." />
-      <input type="button" value="+" />
-      <input type="button" value="-" />
-      <input type="button" value="*" />
-      <input type="button" value="/" />
-      <input type="button" value="%" />
-      <input type="button" value="=" />
-      <input type="button" value="Clear" />
+      <input type="button" value="0" onClick={handleClick} />
+      <input type="button" value="." onClick={handleClick} />
+      <input type="button" value="+" onClick={handleClick} />
+      <input type="button" value="-" onClick={handleClick} />
+      <input type="button" value="*" onClick={handleClick} />
+      <input type="button" value="/" onClick={handleClick} />
+      <input type="button" value="%" onClick={handleClick} />
+
+      <input type="button" value="=" onClick={handleEqual} />
+      <input type="button" value="Clear" onClick={handleClear} />
     </StyledCalculator>
   );
 }
