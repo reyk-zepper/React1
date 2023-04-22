214fac8118fe06b831ea00ffddf2b64652814bd2
Reyk Zepper <reyk.zepper@gmail.com>
2023-04-22 10:55:24 +0200

    add StyledCalculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,8 +1,9 @@
 import React from "react";
+import styled from "styled-components";
 
 export default function Calculator() {
   return (
-    <div>
+    <StyledCalculator>
       <input type="text" placeholder="0" id="answer" />
       <input type="button" value="9" />
       <input type="button" value="8" />
@@ -20,8 +21,50 @@ export default function Calculator() {
       <input type="button" value="-" />
       <input type="button" value="*" />
       <input type="button" value="/" />
+      <input type="button" value="%" />
       <input type="button" value="=" />
       <input type="button" value="Clear" />
-    </div>
+    </StyledCalculator>
   );
 }
+
+const StyledCalculator = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(6, 1fr);
+  grid-gap: 1rem;
+  width: 100vw;
+  height: 80vh;
+  background-color: #f5f5f5;
+  input {
+    border: none;
+    border-radius: 0.5rem;
+    font-size: 2rem;
+    cursor: pointer;
+    &:hover {
+      background-color: #e0e0e0;
+    }
+  }
+  #answer {
+    grid-column: 1 / -1;
+    font-size: 3rem;
+    text-align: right;
+    padding: 1rem;
+  }
+  input[value="="] {
+    grid-column: 1 / -1;
+    background-color: #ff9800;
+    color: white;
+    &:hover {
+      background-color: #f57c00;
+    }
+  }
+  input[value="Clear"] {
+    grid-column: 1 / -1;
+    background-color: #f44336;
+    color: white;
+    &:hover {
+      background-color: #d32f2f;
+    }
+  }
+`;
